3003f92beb8977939ab690827ea9d17945bdaefc
Anthony Cuellar <anthony.cuellar@sjsu.edu>
2024-12-30 13:25:50 -0800

    Initial Project Filesystem Setup

---
+++ clean_my_air_esp32/main/clean_my_air_esp32.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+void app_main(void)
+{
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+  
+    while(true) {
+        gpio_set_level(GPIO_NUM_2, 0);
+        vTaskDelay(100);
+        gpio_set_level(GPIO_NUM_2, 1);
+        vTaskDelay(100);
+    }
+    
+
+}
